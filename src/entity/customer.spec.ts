7d4718bcf1ff1316bcd7dafb7883b31174ce7050
fernandocosta <fernandocostadev98@gmail.com>
2023-01-21 13:46:07 -0300

    :sparkles: (add reward points tests to customer entity)

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -46,4 +46,11 @@ describe('Customer unity tests', () => {
       customer.activate();
     }).toThrowError('Address is mandatory to activate a customer');
   });
+  it('should add reward points', () => {
+    const customer = new Customer('1', 'Customer 1');
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+  });
 });
